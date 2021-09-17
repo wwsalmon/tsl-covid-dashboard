8c6d27740812678af1c536a6a348043342e28f04
Samson Zhang <wwsamson@yahoo.com>
2021-09-17 16:45:50 -0700

    clear out mongodb and nextauth lol

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,5 +1,4 @@
 import {useSession} from "next-auth/client";
-import Button from "./Button";
 import Container from "./headless/Container";
 import {useRouter} from "next/router";
 
@@ -11,17 +10,6 @@ export default function Navbar() {
         <div className="w-full sticky top-0">
             <Container className="flex items-center my-4" width="full">
                 <p>YourApp</p>
-                <div className="ml-auto">
-                    {(session && router.route !== "/") ? (
-                        <img
-                            src={session.user.image}
-                            alt={`Profile picture of ${session.user.name}`}
-                            className="w-8 h-8 rounded-full"
-                        />
-                    ) : (
-                        <Button href="/auth/signin">Sign in</Button>
-                    )}
-                </div>
             </Container>
         </div>
     );
