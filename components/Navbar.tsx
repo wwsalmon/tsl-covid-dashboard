b3d8134de0d8ca306dfc18336bb91846c496ec68
Samson Zhang <wwsamson@yahoo.com>
2021-09-18 10:32:15 -0700

    make navbar mobile responsive

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,5 +1,8 @@
 import {useRouter} from "next/router";
 import Link from "next/link";
+import Button from "./headless/Button";
+import {FiMenu, FiX} from "react-icons/fi";
+import {useState} from "react";
 
 export default function Navbar() {
     const router = useRouter();
@@ -13,22 +16,43 @@ export default function Navbar() {
         "/": "50,116,190",
     }[router.asPath];
 
+    const [menuOpen, setMenuOpen] = useState<boolean>(false);
+
     return (
         <>
-            <div className="w-full sticky top-0 h-16 flex items-center px-4 z-10">
+            <div className="w-full sticky top-0 h-16 flex items-center px-4 z-10 bg-white">
                 <a href="https://tsl.news">
-                    <img src="/tsl.svg" alt="TSL Logo" className="h-3"/>
+                    <img src="/tsl.svg" alt="TSL Wordmark" className="h-3 hidden sm:block"/>
+                    <img src="/tsl-logo.svg" alt="TSL Logo" className="h-5 sm:hidden"/>
                 </a>
                 <Link href="/about">
-                    <a className="ml-auto">About</a>
+                    <a className="ml-auto hidden sm:block">About</a>
                 </Link>
                 <Link href="/">
-                    <a className="absolute left-1/2 font-bold text-lg" style={{transform: "translateX(-50%)"}}>5C COVID Dashboard</a>
+                    <a
+                        className="absolute left-1/2 font-bold sm:text-lg whitespace-nowrap"
+                        style={{transform: "translateX(-50%)"}}
+                    >5C COVID Dashboard</a>
                 </Link>
+                <Button containerClassName="ml-auto" onClick={() => setMenuOpen(!menuOpen)}>
+                    <FiMenu/>
+                </Button>
+                <div className="fixed h-full w-72 top-0 right-0 transition-all bg-white shadow-md flex flex-col items-end justify-center p-4 z-30" style={{marginRight: menuOpen ? "0" : "-100%"}}>
+                    <div className="absolute top-4 right-4">
+                        <Button onClick={() => setMenuOpen(false)}>
+                            <FiX/>
+                        </Button>
+                    </div>
+                    <Button containerClassName="py-2 text-xl" href="About">About this dashboard</Button>
+                    <Button containerClassName="py-2 text-xl" href="About">The Student Life home</Button>
+                    <p className="absolute bottom-4 right-4 text-gray-500 text-sm">
+                        Developed by Samson Zhang
+                    </p>
+                </div>
             </div>
             {themeRGBColor && (
                 <div
-                    className="fixed top-0 left-0 w-full h-16 z-0 pointer-events-none"
+                    className="fixed top-0 left-0 w-full h-16 z-20 pointer-events-none"
                     style={{background: `linear-gradient(180deg, rgba(${themeRGBColor},0.125) 0%, rgba(${themeRGBColor},0.0625) 25%, rgba(${themeRGBColor},0) 100%)`}}
                 />
             )}
