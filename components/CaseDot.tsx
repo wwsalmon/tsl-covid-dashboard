63b326ebfbe0006a6cfd32f72a06e199113d14df
Samson Zhang <wwsamson@yahoo.com>
2021-09-19 14:04:58 -0700

    make casedot into link

--- components/CaseDot.tsx
+++ components/CaseDot.tsx
@@ -1,23 +1,26 @@
 import {CaseItem} from "../utils/types";
 import getBgClass, {getBorderClass, getTextClass} from "../utils/getBgClass";
+import Link from "next/link";
 
 const CaseDot = (props: {case: CaseItem}) => (
-    <div
-        className={`
+    <Link href={`/${props.case.school}`}>
+        <a
+            className={`
             w-10 h-10 mb-5 mr-5 rounded-full flex items-center justify-center
             ${(props.case.isNoReport || props.case.isNoCases)
                 ? `opacity-50 border-2 ${props.case.isNoReport ? "border-dashed" : ""} ${getBorderClass(props.case.school)}`
                 : getBgClass(props.case.school)
             }
         `}
-    >
-        {props.case.isEmployee && (
-            <span className="text-white opacity-75 font-bold">E</span>
-        )}
-        {(props.case.isNoReport || props.case.isNoCases) && (
-            <span className={`font-bold ${getTextClass(props.case.school)}`}>{props.case.isNoReport ? "N" : 0}</span>
-        )}
-    </div>
+        >
+            {props.case.isEmployee && (
+                <span className="text-white opacity-75 font-bold">E</span>
+            )}
+            {(props.case.isNoReport || props.case.isNoCases) && (
+                <span className={`font-bold ${getTextClass(props.case.school)}`}>{props.case.isNoReport ? "N" : 0}</span>
+            )}
+        </a>
+    </Link>
 );
 
 export default CaseDot;
