d6ef8d3ca5306fe968bc3787fa41feeea2a26d70
Samson Zhang <wwsamson@yahoo.com>
2021-09-30 20:29:23 -0700

    fix data description on time message

--- components/DataDescription.tsx
+++ components/DataDescription.tsx
@@ -33,24 +33,17 @@ export default function DataDescription({school}: {
     }[school];
 
     const allReportedSchoolItems =  data.filter(d => d.school === school && d.reported !== "historic");
-    const numReported = allReportedSchoolItems.length;
     const itemsReportedOnTime = allReportedSchoolItems.filter(d => {
         const weekStartDate = new Date(d.weekStart);
         const weekEndDate = addDays(weekStartDate, 6);
         const reportedDate = new Date(d.reported);
         return +reportedDate <= +weekEndDate;
     });
-    const numReportedOnTime = itemsReportedOnTime.length;
-    const percentageOnTime = numReported === 0 ? 0 : (numReportedOnTime / numReported * 100).toFixed(2);
 
     return (
         <>
             <p>Source: <a href={schoolInfo.link}>{schoolInfo.title}</a></p>
-            {school === "scripps" ? (
-                <p>As of {format(addDays(new Date(), -(getISODay(new Date())) % 7), "MMMM d, yyyy")}, Scripps has only reported the results for one week of testing: of 229 students and 252 employees from September 3 to September 10. According to the website, 4,722 tests have been conducted since January 2021, but no other results are available.</p>
-            ) : (
-                <p>According to {getSchoolShortName(school)}'s website, {getSchoolShortName(school)} publishes COVID data {schoolInfo.reportingFrequency || "weekly"}. Since September 19, 2021, we have found {getSchoolShortName(school)}'s reporting to be <b>on-time {percentageOnTime}% of the time.</b> The dashboard displays COVID testing and positivity on a {schoolInfo.weeklyBasis || "Monday-Sunday"} weekly basis.</p>
-            )}
+            <p>According to {getSchoolShortName(school)}'s website, {getSchoolShortName(school)} publishes COVID data {schoolInfo.reportingFrequency || "weekly"}. The dashboard displays COVID testing and positivity on a {schoolInfo.weeklyBasis || "Monday-Sunday"} weekly basis.</p>
         </>
     );
 }
